beb48507f88defd4d46ab8d57a92553da35e07fe
Ashish Trivedi <atrivedi@tsavoritesi.com>
2024-08-05 16:45:19 -0700

    @FIR-3: First draft of fully functional single threaded application which does invoke main and does a math function to do math. Have verified in DV environment that the hello_world main is being invoked and doing the math.

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -8,7 +8,15 @@
 
 int main(void)
 {
+#ifndef CONFIG_XTENSA_TENSILICA_NX
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
+#else
+	int a = 0, b = 10;
+	for (a = 0; a < 12; a++){
+	    if (a == b){
+		break;
+	    }
+	}
+#endif
 	return 0;
 }
